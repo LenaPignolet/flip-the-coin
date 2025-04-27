496bf9939eacd2cbd4b8885a0d323b6cbc2f709f
LenaPignolet <lena.pignolet.pro@gmail.com>
2025-04-27 16:34:30 +0200

    Add js files and update class names

---
+++ assets/js/coin-flip.js
@@ -0,0 +1,8 @@
+const flipImage = document.querySelector('');
+const flipBtn = document.querySelector('game_btn');
+
+flipBtn.addEventListener('click', () => {
+    const result = Math.random() < 0.5 ? "Heads" : "Tails";
+
+
+})
