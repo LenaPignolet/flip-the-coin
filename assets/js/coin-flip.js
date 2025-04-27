1145ea68fb401e849a887e27b7d1a25b3b00737b
LenaPignolet <lena.pignolet.pro@gmail.com>
2025-04-27 17:08:00 +0200

    Add js script

--- assets/js/coin-flip.js
+++ assets/js/coin-flip.js
@@ -1,8 +1,28 @@
-const flipImage = document.querySelector('');
-const flipBtn = document.querySelector('game_btn');
+const imageHeads = document.querySelector('.game__image-heads');
+const imageTails = document.querySelector('.game__image-tails');
+const flipBtn = document.querySelector('.game__btn');
+const resultDisplay = document.querySelector('.game__result');
 
-flipBtn.addEventListener('click', () => {
+function flipCoin() {
     const result = Math.random() < 0.5 ? "Heads" : "Tails";
+    displayResult(result);
+    updateImages(result);
+}
 
+function displayResult(result) {
+    resultDisplay.textContent = result;
+}
 
+function updateImages(result) {
+    const isHeads = result === "Heads";
+
+    imageHeads.classList.toggle('game__image--is-visible', isHeads);
+    imageHeads.classList.toggle('game__image--is-hidden', !isHeads);
+
+    imageTails.classList.toggle('game__image--is-visible', !isHeads);
+    imageTails.classList.toggle('game__image--is-hidden', isHeads);
+}
+
+[imageHeads, imageTails, flipBtn].forEach(element => {
+    element.addEventListener("click", flipCoin);
 })
